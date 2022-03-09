c5d261c4c83557162e262ea1c68b83d3134ebf93
unknown <nvdatdev@gmail.com>
2022-03-10 05:56:10 +0700

    rechange nuxt conflig

--- components/Shared/Footer.vue
+++ components/Shared/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="pt-4 my-md-5 pt-md-5 border-top" style="display: inline">
     <div class="row">
-      <div class="col-12 col-md">
+      <div class="col-12 col-md" style="padding-top: 50px">
         <img
           class="mb-2"
           src="/docs/4.6/assets/brand/bootstrap-solid.svg"
